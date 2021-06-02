99c9f12907648342619a5e419e18896c8ae88515
marceloguterres <m.guterres@gmail.com>
2021-06-01 21:50:39 -0300

    Add files via upload

--- app_mipx.py
+++ app_mipx.py
@@ -12,7 +12,17 @@ import plotly.express as px
 
 # -- Set page config
 
-st.set_page_config(layout="wide")
+
+list_mults = ["eDN_produto", "eD_adicionado", "eDN_adicionado", "tDN_adicionado",
+              "eD_renda", "eDN_renda", "tDN_renda",	"eD_emprego",	"eDN_emprego",
+              "tDN_emprego"]
+
+
+df_mipita_all = pd.read_pickle("df_mipita_all.bz2")
+
+
+
+option = st.sidebar.selectbox('Select the multiplier?', list_mults)
 
 st.apptitle = 'MIPITA v1.0'
 
@@ -20,23 +30,20 @@ st.title('PROJETO IMPACTO O1-E7-IMPACTO')
 
 st.markdown(""" 
             * APP MIPITA v 1.0
-            * Data da versão: 2021.06.01   
+            * Data da versão: 2021.06.01
+            * Estudo de Caso: Região Litorânea de Santa Catarina
+            * Estudo de Caso: Região metropolitana de São Paulo
             """)
+            
+            
+
+st.write(df_mipita_all)
+
+st.write('You selected:', option)
 
 
+fig = px.line(df_mipita_all, x="ano", y= option, color="icao")
 
-st.write("Here's our first attempt at using data to create a table:")
-
-
-
-df_mipita_all = pd.read_pickle("df_mipita_all.bz2")
-
-
-df_mipita_all
-
-
-fig = px.line(df_mipita_all, x="ano", y="eDN_produto", color="icao")
-
-st.plotly_chart(fig)
+st.write(fig)
 
 
